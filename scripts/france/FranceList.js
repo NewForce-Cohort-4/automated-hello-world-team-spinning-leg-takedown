b2d1a301517976d797e4d9dc3454da0e8fda8ada
Detective-Coder <aaronfrankenfield@gmail.com>
2021-01-21 13:09:30 -0500

    finished lists, added image and h1

--- scripts/france/FranceList.js
+++ scripts/france/FranceList.js
@@ -5,6 +5,7 @@ let franceContainer = document.querySelector("#main-content");
 
 let cityCollectionArray = useCityCollection();
 let celebrityCollectionArray = useCelebrityCollection();
+let landmarkCollectionArray = useLandmarkCollection();
 
 function buildCityListHTML(cityCollection){
   let cityListHTML = "";
@@ -26,17 +27,30 @@ function buildCelebrityListHTML(celebrityCollection){
   return celebrityListHTML;
 }
 
+function buildLandmarkListHTML(landmarkCollection){
+  let landmarkListHTML = "";
+
+  for(let i = 0; i < landmarkCollection.length; i++){
+    landmarkListHTML += France(landmarkCollection[i]);
+  }
+
+  return landmarkListHTML;
+}
+
 export function FranceList() {
   const franceCityCollection = useCityCollection();
   const franceCelebrityCollection = useCelebrityCollection();
+  const franceLandmarkCollection = useLandmarkCollection();
 
   const franceCityHTML = buildCityListHTML(franceCityCollection);
   const franceCelebrityHTML = buildCelebrityListHTML(franceCelebrityCollection);
+  const franceLandmarkHTML = buildLandmarkListHTML(franceLandmarkCollection);
 
 
   franceContainer.innerHTML = `
   <h2>Cities</h2>${franceCityHTML}
   <h2>Celebrities</h2>${franceCelebrityHTML}
+  <h2>Landmarks</h2>${franceLandmarkHTML}
 `
 }
 
