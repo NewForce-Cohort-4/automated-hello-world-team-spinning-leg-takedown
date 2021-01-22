d5368f0b2e591cabe19743d710ce94ad84b0a0f0
Mandy <drgnfli6420@yahoo.com>
2021-01-22 09:58:06 -0500

    css the lists to look perfect

--- scripts/zambia/zambia.js
+++ scripts/zambia/zambia.js
@@ -1,8 +1,8 @@
 export function Zambia(zambiaArray) {
   return `
-    <div class ="zambia-card__text">
+    
         <ul>
             <li>${zambiaArray}</li>
         </ul>
-   </div>`;
+   `;
 }
